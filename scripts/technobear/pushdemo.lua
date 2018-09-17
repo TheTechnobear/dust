013c1c9461750082558e34756b263af3faf7dd2b
Mark Harris <mharris@technobear.com>
2018-09-17 19:22:10 +0200

    update pushdemo

--- scripts/technobear/pushdemo.lua
+++ scripts/technobear/pushdemo.lua
@@ -1,7 +1,6 @@
--- draw demo
---
--- code shows all of the screen
--- drawing functions
+-- push 2 native test
+-- press device to switch between modes
+-- shows graphcis, animation, control
 
 engine.name = 'TestSine'
 
@@ -10,8 +9,9 @@ local metro = require 'metro'
 local i = 0
 local ctl = nil
 local dev = nil
-local mididev = nil
+-- local mididev = nil
 local mode = 0
+-- local imidi = false
 
 function init()
   engine.amp(0)
@@ -20,8 +20,10 @@ function init()
   params:add_number("pw",0,1,1)
   params:add_number("cutoff",1,20000,500)
   params:add_number("res",0,100,48)
-  params:add_number("attack",0,240,48)
-  params:add_number("decay",0,240,48)
+  params:add_number("attack",0,100,0)
+  params:add_number("decay",0,100,25)
+  params:add_number("sustain",0,100,100)
+  params:add_number("release",0,100,25)
     -- dev = push2.devices[2].dev
   for i = 0, 10 do 
     if push2.devices[i] ~= nil then 
@@ -33,6 +35,9 @@ function init()
    return
   end
 
+  p2_clear(dev)
+  p2_update(dev)
+
   clk = metro.alloc()
   clk.time = 1/15
   clk.callback = function(stage)
@@ -40,14 +45,12 @@ function init()
   end
   clk:start()
 
-  print("adsakfalkfa")
   p2_button_state(dev,110,127)
 end
 
 function cleanup()
   metro.free(cld.id)
 end
-
 
 
 
@@ -77,20 +80,32 @@ function redraw()
 
   i = i + 1
   if mode == 0 then 
-    anim(dev)
-  elseif mode == 1 then
     drawall(dev)
+  elseif mode == 1 then
+    anim(dev)
   else
     pdraw(dev)
   end
   p2_update(dev);
+  
+  -- --something calls screen_update after init and midi.add!
+  -- if not imidi and mididev then
+  --   imidi = true
+  --   initmidi()
+  -- end
+    
 end
 
 function pdraw(dev) 
+  p2_clear(dev)
+  p2_font_size(dev,20)
+  p2_font_face(dev,9)
   for param_index = 1,params.count do
-    p2_move(dev,param_index *100,30)
+    p2_colour(dev, 0.5,0.5, 1)
+    p2_move(dev,5 + (param_index-1) * 125 ,30)
     p2_text(dev,params:get_name(param_index))
-    p2_move(dev,param_index *100,50)
+    p2_colour(dev, 1, 1, 1)
+    p2_move(dev,5 +(param_index-1) * 125,50)
     p2_text(dev,params:string(param_index))
   end
 end
@@ -115,32 +130,32 @@ function drawall(dev)
 
   p2_line_width(dev,2.0)
   p2_move(dev,0,0)
-  p2_line(dev,10,20)
+  p2_line(dev,100,200)
   p2_stroke(dev)
 
-  p2_arc(dev,20,0,10,0,math.pi*0.8)
+  p2_arc(dev,0,100,100,0,math.pi*0.8)
   p2_stroke(dev)
 
-  p2_rect(dev,30,10,15,20)
+  p2_rect(dev,230,100,150,200)
   p2_colour(dev, 0,1,0)
   p2_stroke(dev)
 
-  p2_move(dev,50,0)
-  p2_curve(dev,50,20,60,0,70,10)
+  p2_move(dev,550,0)
+  p2_curve(dev,150,120,600,0,700,10)
   p2_colour(dev, 0,0,1)
   p2_stroke(dev)
-  p2_move(dev,60,20)
-
-  p2_line(dev,80,10)
-  p2_line(dev,70,40)
+  
+  p2_move(dev,700,0)
+  p2_line(dev,800,50)
+  p2_line(dev,750,100)
   p2_close(dev)
   p2_colour(dev, 1,1,1)
   p2_fill(dev)
 
-  p2_circle(dev,100,20,10)
+  p2_circle(dev,400,120,100)
   p2_stroke(dev)
 
-  p2_move(dev,0,100)
+  p2_move(dev,300,100)
   p2_font_face(dev,4)
   p2_font_size(dev,40)
 
@@ -151,24 +166,17 @@ function drawall(dev)
   p2_text(dev,"new!")
 
   p2_colour(dev, 1,0,0)
-  p2_move(dev,63,50)
-  p2_font_face(dev,0)
-  p2_font_size(dev,8)
+
+
+  p2_move(dev,763,50)
+  p2_font_face(dev,4)
+  p2_font_size(dev,20)
   p2_text_center(dev,"center")
+  
   -- draw right aligned text
-  p2_move(dev,127,63)
+  p2_move(dev,227,63)
   p2_text_right(dev,"1992")
 
-end
-
-local function note_on(note, vel)
-  local data = {0x90,note,vel}
-  midi_send(mididev,data)
-end
-
-local function note_off(note, vel)
-  local data = {0x80,note,vel}
-  midi_send(mididev,data)
 end
 
 
@@ -178,39 +186,14 @@ function key(n,z)
     mode = (mode + 1) % 3
     p2_clear(dev)
     p2_update(dev)
-    print(params.count)
   end 
 end
 
 function enc(n,d)
-  print("enc"..n..":"..d)
+    local param = n
+    if n <= params.count then
+      local name = params:get_name(param)
+      params:delta(name,d)
+    end
 end
 
-
-
-
-local function midi_event(data)
-  if not data[1] then return end
-
-  local status  = data[1] 
-  -- local status  = (data[1] & 0x0F)
-  if status == 0x90 then
-    note_on(data[2], data[3])
-  elseif status == 0x80 then
-    note_off(data[2],data[3])
-  end
-end
-
-local function initmidi(dev)
-  for i=9,7 do
-  local data = {0xB0,i+102,i}
-    midi_send(dev,data)
-  end
---  midi_send(dev, {0xB0,110,127})
-end
-
-midi.add = function(dev)
-  dev.event = midi_event
-  mididev = dev.dev
-  initmidi(dev.dev)
-end
